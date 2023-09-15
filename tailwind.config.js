382f5c02b4e7aa9063b6b1bd3db03bcd2a873bab
Rob <robzlegz@gmail.com>
2023-09-15 13:38:01 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: "#FF5C00"
+        },
+        primary: {
+          100: "#68C8F1",
+          100: "#FFC956",
+          100: "#6A6FF1",
+          100: "#FE76A8",
+          100: "#C994DC",
+          100: "#FF814B",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
